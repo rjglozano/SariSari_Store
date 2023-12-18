3800b31f30f6c21764efc1387c238cc18eaa7661
ryanlozano07 <ryan.rjl38@gmail.com>
2023-12-19 02:58:19 +0800

    loading items

--- app/items/page.tsx
+++ app/items/page.tsx
@@ -1,38 +1,12 @@
 'use client'
 
 import React, { useState } from 'react';
-import Modal from '@/components/Modal';
 import Items from '@/components/Items';
-import SearchBar from '@/components/SearchBar';
-
 const ItemsPage: React.FC = () => {
-  const [isModalOpen, setModalOpen] = useState(false);
-
-  const handleOpenModal = () => {
-    setModalOpen(true);
-  };
-
-  const handleCloseModal = () => {
-    setModalOpen(false);
-  };
-
- 
 
   return (
     <div className='min-h-screen bg-zinc-200'>
-      <div className='flex justify-between items-center'>
-        <button
-          onClick={handleOpenModal}
-          className="bg-red-400 font-semibold text-white px-4 py-2 rounded-md hover:bg-red-500 m-5 md:w-1/6 w-1/"
-        >
-          Add Item
-        </button>
-        <SearchBar />
-      </div>
-
-      <Modal isOpen={isModalOpen} onClose={handleCloseModal} />
       <Items />
-     
     </div>
   );
 };
--- components/Items.tsx
+++ components/Items.tsx
@@ -2,6 +2,10 @@
 
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import Image from "next/image";
+import SpinnerTwo from "./SpinnerTwo";
+import SearchBar from "./SearchBar";
+import Modal from "./Modal";
 
 interface Item {
   id: string;
@@ -11,10 +15,31 @@ interface Item {
   image: string;
 }
 
+//GENERICS
+
+// interface testMe<T extends object>{
+//   id: string;
+//   username: string;
+//   extra: T[]
+// }
+
+// TYPE
+
+// export type PostProps = {id: number, username: string};
+
 
 const Items = () => {
   const [items, setItems] = useState<Item[]>([]);
-  
+  const [loading, setLoading] = useState(true);
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -22,31 +47,62 @@ const Items = () => {
         const response = await fetch("/api/items/");
         const data = await response.json();
         setItems(data);
+        setLoading(false);
       } catch (error) {
         console.error("Error fetching data:", error);
-      } 
-  }
+        setLoading(false);
+      }
+    };
 
     fetchData();
-  }, []); 
+  }, []);
 
   return (
     <div className="w-full">
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5 mx-5 place-items-center	">
-      {items.map((item) => (
-          <div className="bg-red-400 rounded-md shadow-lg overflow-hidden sm:w-full cursor-pointer hover:scale-105 bg-gradient-to-t from-red-500 to-zinc-50 mb-5" key={item.id}>
-            <img className="rounded-t-md" src={item.image} alt={item.name} />
-            <div className="font-semibold text-center mt-2 text-sm sm:text-lg md:text-2xl text-white">{item.name}</div>
-            <div className="text-sm sm:text-lg md:text-xl text-white text-center p-2 rounded-md mr-4">₱{parseFloat(item.price).toFixed(2)}</div>
-            <button className="p-2 bg-white text-gray-500 w-full font-semibold hover:bg-gray-100">
-              <Link href={`/items/${item.id}`}>
-                View Details
-              </Link>
-              </button>
       
+   
+
+      {loading ? (
+        <div className="text-center"><SpinnerTwo /></div>
+      ) : (
+        <div className="bg-red-100">    
+          <div className='flex justify-between items-center'>
+            <button
+              onClick={handleOpenModal}
+              className="bg-red-400 font-semibold text-white px-4 py-2 rounded-md hover:bg-red-500 m-5 md:w-1/6 w-1/"
+            >
+              Add Item
+            </button>
+            <SearchBar />
           </div>
-        ))}
-      </div>
+
+          <Modal isOpen={isModalOpen} onClose={handleCloseModal} /> 
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5 mx-5 place-items-center">
+          {items.map((item) => (
+            <div
+              className="bg-red-400 rounded-md shadow-lg overflow-hidden sm:w-full cursor-pointer hover:scale-105 bg-gradient-to-t from-red-500 to-zinc-50 mb-5"
+              key={item.id}
+            >
+              <Image
+                src={item.image}
+              
+                alt={item.name}
+                width={300}
+                height={400}
+                className="rounded-t-md"
+              />
+              <div className="font-semibold text-center mt-2 text-sm sm:text-lg md:text-2xl text-white">{item.name}</div>
+              <div className="text-sm sm:text-lg md:text-xl text-white text-center p-2 rounded-md mr-4">₱{parseFloat(item.price).toFixed(2)}</div>
+              <button className="p-2 bg-white text-gray-500 w-full font-semibold hover:bg-gray-100">
+                <Link href={`/items/${item.id}`}>
+                  View Details
+                </Link>
+              </button>
+            </div>
+          ))}
+        </div>
+        </div>   
+      )}
     </div>
   );
 };
